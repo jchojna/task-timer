f4f1255e415a47a8be57cabe76dd2ce034b4672a
Jakub Chojna <jakub.chojna@gmail.com>
2019-10-19 00:31:56 +0200

    feat: add section's sliding functionality

--- js/main.js
+++ js/main.js
@@ -1,30 +1,40 @@
-const goToSection = (e) => {
+const slideSection = (e) => {
 
   switch(e.target) {
-
     case rightButton:
-      console.log(e.target);
+      taskSection.className = 'task task--js slideOutLeft';
+      timeSection.className = 'time time--js time--visible slideInRight';
       break;
-
     case leftButton:
-      console.log(e.target);
+      timeSection.className = 'time time--js slideOutRight';
+      taskSection.className = 'task task--js task--visible slideInLeft';
       break;
-
+    case startButton:
+      timeSection.className = 'time time--js slideOutLeft';
+      timerSection.className = 'timer timer--js timer--visible slideInRight';
+      break;
     case timerStop:
-      console.log(e.target);
+      timerSection.className = 'timer timer--js slideOutLeft';
+      taskSection.className = 'task task--js task--visible slideInRight';
       break;
-
+    default: false;
   }
 }
 
 //////////////////////////////////////////////////////////////////// VARIABLES 
 
+const taskSection = document.querySelector('.task--js');
+const timeSection = document.querySelector('.time--js');
+const timerSection = document.querySelector('.timer--js');
+
 const rightButton = document.querySelector('.task__button--js-right');
-const leftButton = document.querySelector('.task__button--js-left');
+const leftButton = document.querySelector('.time__button--js-left');
+const startButton = document.querySelector('.time__start--js');
 const timerStop = document.querySelector('.timer__button--js-stop');
 
 /////////////////////////////////////////////////////////////////////// EVENTS 
 
-rightButton.addEventListener('click', goToSection);
-leftButton.addEventListener('click', goToSection);
-timerStop.addEventListener('click', goToSection);
+rightButton.addEventListener('click', slideSection);
+leftButton.addEventListener('click', slideSection);
+startButton.addEventListener('click', slideSection);
+timerStop.addEventListener('click', slideSection);
