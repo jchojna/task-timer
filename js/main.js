c27d22d780bfce384c631f814b3463800b7f79cf
Jakub Chojna <jakub.chojna@gmail.com>
2019-10-19 01:16:14 +0200

    feat: add initial input handlers

--- js/main.js
+++ js/main.js
@@ -21,16 +21,47 @@ const slideSection = (e) => {
   }
 }
 
+const handleTaskName = (e) => {
+  taskName = `"${e.target.value}"`;
+  timerHeading.textContent = taskName;
+  console.log('taskName', taskName);
+}
+
+const handleTaskTime = (e) => {
+  taskTime = e.target.value;
+  console.log('taskTime', taskTime);
+}
+
+const handleTaskBreakTime = (e) => {
+  taskBreakTime = e.target.value;
+  console.log('taskBreakTime', taskBreakTime);
+}
+
+
+
+
+
+
+
+
 //////////////////////////////////////////////////////////////////// VARIABLES 
-
+// TASK
 const taskSection = document.querySelector('.task--js');
-const timeSection = document.querySelector('.time--js');
-const timerSection = document.querySelector('.timer--js');
-
 const rightButton = document.querySelector('.task__button--js-right');
+const taskInput = document.querySelector('.task__input--js');
+let taskName = "";
+// TIME
+const timeSection = document.querySelector('.time--js');
 const leftButton = document.querySelector('.time__button--js-left');
 const startButton = document.querySelector('.time__start--js');
+const timeInput = document.querySelector('.time__input--js-time');
+const breakTimeInput = document.querySelector('.time__input--js-break-time');
+let taskTime = "";
+let taskBreakTime = "";
+// TIMER
+const timerSection = document.querySelector('.timer--js');
 const timerStop = document.querySelector('.timer__button--js-stop');
+const timerHeading = document.querySelector('.timer__heading--js');
 
 /////////////////////////////////////////////////////////////////////// EVENTS 
 
@@ -38,3 +69,7 @@ rightButton.addEventListener('click', slideSection);
 leftButton.addEventListener('click', slideSection);
 startButton.addEventListener('click', slideSection);
 timerStop.addEventListener('click', slideSection);
+
+taskInput.addEventListener('change', handleTaskName);
+timeInput.addEventListener('change', handleTaskTime);
+breakTimeInput.addEventListener('change', handleTaskBreakTime);
