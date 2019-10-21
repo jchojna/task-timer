5b3b245acde9571ae23fc02e5869b3d1c7cc7594
Jakub Chojna <jakub.chojna@gmail.com>
2019-10-21 20:12:26 +0200

    feat: display overall time in outro section

--- js/main.js
+++ js/main.js
@@ -1,61 +1,70 @@
 class Task {
   constructor(time) {
     this.name = "",
-    this.timeElapsed = 0,
-    this.timeRemaining = time,
+    this.workTimeElapsed = 0,
+    this.workTimeRemaining = time,
     this.previousTime = 0,
-    this.breakElapsed = 0,
-    this.isRunning = false,
+    this.breakTimeElapsed = 0,
+    this.isWork = false,
     this.isBreak = false
   }
 
-  get timeElapsedArray() {
-    return this._timeElapsedArray;
+  get workTimeElapsedArray() {
+    return this._workTimeElapsedArray;
   }
-  set timeElapsedArray(time) {                                 // ! TO REFACTOR
-    this._timeElapsedArray = [
+  set workTimeElapsedArray(time) {                                 // ! TO REFACTOR
+    this._workTimeElapsedArray = [
       makeTwoDigits(Math.floor(time / 60000)),
       makeTwoDigits(Math.floor(time / 1000 % 60)),
       makeTwoDigits(Math.floor(time / 10 % 100))
     ];
   }
-  get timeRemainingArray() {
-    return this._timeRemainingArray;
+  get workTimeRemainingArray() {
+    return this._workTimeRemainingArray;
   }
-  set timeRemainingArray(time) {                               // ! TO REFACTOR
-    this._timeRemainingArray = [
+  set workTimeRemainingArray(time) {                               // ! TO REFACTOR
+    this._workTimeRemainingArray = [
       makeTwoDigits(Math.floor(time / 60000)),
       makeTwoDigits(Math.floor(time / 1000 % 60)),
       makeTwoDigits(Math.floor(time / 10 % 100))
     ];
   }
-  get breakElapsedArray() {
-    return this._breakElapsedArray;
+  get breakTimeElapsedArray() {
+    return this._breakTimeElapsedArray;
   }
-  set breakElapsedArray(time) {                                 // ! TO REFACTOR
-    this._breakElapsedArray = [
+  set breakTimeElapsedArray(time) {                                 // ! TO REFACTOR
+    this._breakTimeElapsedArray = [
       makeTwoDigits(Math.floor(time / 60000)),
       makeTwoDigits(Math.floor(time / 1000 % 60)),
       makeTwoDigits(Math.floor(time / 10 % 100))
+    ];
+  }
+  get overallTimeArray() {
+    return this._overallTimeArray;
+  }
+  set overallTimeArray(time) {                                 // ! TO REFACTOR
+    this._overallTimeArray = [
+      Math.floor(time / 60000),
+      Math.floor(time / 1000 % 60)
     ];
   }
 }
 // F0 ////////////////////////////////////////////////////////////// COUNTDOWN 
 
 const countdown = () => {
-  if (task.isRunning) {
+  if (task.isWork) {
     let {
-      timeElapsed,
-      timeRemaining,
+      workTimeElapsed,
+      workTimeRemaining,
       timeTotal,
       previousTime
     } = task;
     
     // when countdown finishes
-    if (timeElapsed >= timeTotal) {
-      task.timeElapsed, timeElapsed = timeTotal;
-      task.timeRemaining, timeRemaining = 0;
-      task.isRunning = false;
+    if (workTimeElapsed >= timeTotal) {
+      task.workTimeElapsed, workTimeElapsed = timeTotal;
+      task.workTimeRemaining, workTimeRemaining = 0;
+      task.isWork = false;
       startButton.disable = false;
       startButton.classList.remove('time__start--disabled');
       togglePlayPauseButton('play');
@@ -64,6 +73,8 @@ const countdown = () => {
       timerStop.removeEventListener('click', handleButtons);
       timerPlayPause.removeEventListener('click', handleButtons);
       timerToggle.removeEventListener('click', handleTimerToggle);
+      task.overallTime = task.workTimeElapsed + task.breakTimeElapsed;
+      task.overallTimeArray = task.overallTime;
       clearInterval(intervalWorkId);
       clearInterval(intervalBreakId);
       handleOutro();
@@ -71,28 +82,28 @@ const countdown = () => {
     } else {
       const now = Date.now();
       task.previousTime = now;
-      task.timeElapsed = timeElapsed + (now - previousTime);
+      task.workTimeElapsed = workTimeElapsed + (now - previousTime);
     }
 
-    task.timeRemaining = timeTotal - timeElapsed;
-    task.timeElapsedArray = timeElapsed;
-    task.timeRemainingArray = timeRemaining;
+    task.workTimeRemaining = timeTotal - workTimeElapsed;
+    task.workTimeElapsedArray = workTimeElapsed;
+    task.workTimeRemainingArray = workTimeRemaining;
 
-    const [eMin, eSec, eCSec] = task.timeElapsedArray;                             // ! TO REFACTOR
+    const [eMin, eSec, eCSec] = task.workTimeElapsedArray;                             // ! TO REFACTOR
     elapsedMin.textContent = eMin;
     elapsedSec.textContent = eSec;
     elapsedCSec.textContent = eCSec;
 
-    const [rMin, rSec, rCSec] = task.timeRemainingArray;                           // ! TO REFACTOR
+    const [rMin, rSec, rCSec] = task.workTimeRemainingArray;                           // ! TO REFACTOR
     remainingMin.textContent = rMin;
     remainingSec.textContent = rSec;
     remainingCSec.textContent = rCSec;
 
-    const percentElapsed = timeElapsed / timeTotal * 100;                           // ! TO REFACTOR
+    const percentElapsed = workTimeElapsed / timeTotal * 100;                           // ! TO REFACTOR
     progressPercentElapsed.textContent = `${Math.round(percentElapsed)} %`;
     progressBarElapsed.style.width = `${percentElapsed}%`;
     
-    const percentRemaining = task.timeRemaining / task.timeTotal * 100;            // ! TO REFACTOR
+    const percentRemaining = task.workTimeRemaining / task.timeTotal * 100;            // ! TO REFACTOR
     progressPercentRemaining.textContent = `${Math.round(percentRemaining)} %`;
     progressBarRemaining.style.width = `${percentRemaining}%`;
   }
@@ -102,18 +113,18 @@ const breakTime = () => {
   if (task.isBreak) {
     const {
       previousTime,
-      breakElapsed
+      breakTimeElapsed
     } = task;
 
     const now = Date.now();
     task.previousTime = now;
-    task.breakElapsed = breakElapsed + (now - previousTime);
+    task.breakTimeElapsed = breakTimeElapsed + (now - previousTime);
 
-    task.breakElapsedArray = task.breakElapsed;
-    const [bMin, bSec, bCSec] = task.breakElapsedArray;                           // ! TO REFACTOR
-    breakElapsedMin.textContent = bMin;
-    breakElapsedSec.textContent = bSec;
-    breakElapsedCSec.textContent = bCSec;
+    task.breakTimeElapsedArray = task.breakTimeElapsed;
+    const [bMin, bSec, bCSec] = task.breakTimeElapsedArray;                           // ! TO REFACTOR
+    breakTimeElapsedMin.textContent = bMin;
+    breakTimeElapsedSec.textContent = bSec;
+    breakTimeElapsedCSec.textContent = bCSec;
   }
 }
 // F0 /////////////////////////////////////////////////////// HANDLE TASK TIME 
@@ -147,21 +158,21 @@ const handleButtons = (e) => {
       if (validateInput('time') && !startButton.disable) {
         timeSection.className = 'time time--js slideOutLeft';
         timerSection.className = 'timer timer--js timer--visible slideInRight';
-        task.isRunning = true;
+        task.isWork = true;
         task.previousTime = Date.now();
         task.timeTotal = setTotalTime();
         if (task.timeTotal <= 0) return;
-        task.timeElapsed = 0;
+        task.workTimeElapsed = 0;
         intervalWorkId = setInterval(() => countdown(), 10);
         startButton.disable = true;
         startButton.classList.add('time__start--disabled');
         task.totalBreaks = 0;
-        task.breakElapsed = 0;
-        task.breakElapsedArray = task.breakElapsed;
-        const [bMin, bSec, bCSec] = task.breakElapsedArray;                           // ! TO REFACTOR
-        breakElapsedMin.textContent = bMin;
-        breakElapsedSec.textContent = bSec;
-        breakElapsedCSec.textContent = bCSec;
+        task.breakTimeElapsed = 0;
+        task.breakTimeElapsedArray = task.breakTimeElapsed;
+        const [bMin, bSec, bCSec] = task.breakTimeElapsedArray;                           // ! TO REFACTOR
+        breakTimeElapsedMin.textContent = bMin;
+        breakTimeElapsedSec.textContent = bSec;
+        breakTimeElapsedCSec.textContent = bCSec;
         togglePlayPauseButton('pause');
         updateBreaksCounter();
         timerStop.addEventListener('click', handleButtons);
@@ -177,9 +188,9 @@ const handleButtons = (e) => {
 
     case timerPlayPause:
       // enabling break mode
-      if (task.isRunning) {
+      if (task.isWork) {
         togglePlayPauseButton('play');
-        task.isRunning = false;
+        task.isWork = false;
         task.isBreak = true;
         task.totalBreaks = task.totalBreaks + 1;
         updateBreaksCounter();
@@ -188,7 +199,7 @@ const handleButtons = (e) => {
       } else {
         togglePlayPauseButton('pause');
         clearInterval(intervalBreakId);
-        task.isRunning = true;
+        task.isWork = true;
         task.isBreak = false;
         task.previousTime = Date.now();
       }
@@ -203,7 +214,7 @@ const handleStopConfirm = (e) => {
 
     case confirmStopButton:
       stopSection.removeEventListener('click', handleStopConfirm);
-      task.isRunning = false;
+      task.isWork = false;
       startButton.disable = false;
       startButton.classList.remove('time__start--disabled');
       togglePlayPauseButton('play');
@@ -285,7 +296,21 @@ const handleTimerToggle = () => {
 }
 
 const handleOutro = () => {
+  const [minutes, seconds] = task.overallTimeArray;
   outroSection.classList.add('outro--visible');
+  outroMessage.innerHTML = `
+  You have finished your task entitled "${task.name}" <br>
+  in
+  ${minutes > 1 ? `${minutes} minutes` : minutes === 1 ? `${minutes} minute` : ``}
+  ${seconds != 0 ? `and` : ``}
+  ${seconds > 1 ? `${seconds} seconds` : seconds === 1 ? `${seconds} second` : ``}. <br>
+  fds
+  `
+
+
+
+
+
 }
 
 //////////////////////////////////////////////////////////////////// VARIABLES 
@@ -320,9 +345,9 @@ const remainingCSec = document.querySelector('.display__container--js-remaining 
 const elapsedMin = document.querySelector('.display__container--js-elapsed .display__time--js-min');
 const elapsedSec = document.querySelector('.display__container--js-elapsed .display__time--js-sec');
 const elapsedCSec = document.querySelector('.display__container--js-elapsed .display__time--js-cSec');
-const breakElapsedMin = document.querySelector('.break__time--js-min');
-const breakElapsedSec = document.querySelector('.break__time--js-sec');
-const breakElapsedCSec = document.querySelector('.break__time--js-cSec');
+const breakTimeElapsedMin = document.querySelector('.break__time--js-min');
+const breakTimeElapsedSec = document.querySelector('.break__time--js-sec');
+const breakTimeElapsedCSec = document.querySelector('.break__time--js-cSec');
 const breaksCounter = document.querySelector('.break__counter--js');
 // PROGRESS BAR
 const progressBar = document.querySelector('.progress__bar--js');
@@ -337,9 +362,12 @@ const confirmStopButton = document.querySelector('.stop__button--js-stop');
 const cancelStopButton = document.querySelector('.stop__button--js-cancel');
 // OUTRO
 const outroSection = document.querySelector('.outro--js');
+const outroMessage = document.querySelector('.outro__message--js');
 
 /////////////////////////////////////////////////////////////////////// EVENTS 
 
 rightButton.addEventListener('click', handleButtons);
 leftButton.addEventListener('click', handleButtons);
 startButton.addEventListener('click', handleButtons);
+
+/* handleOutro(); */
