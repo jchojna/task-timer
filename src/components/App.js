57a700e45ce3bdf27cbb4b67f71abd28ba67f5c8
Jakub Chojna <jakub.chojna@gmail.com>
2019-10-23 23:07:05 +0200

    chore: organize file structure and update file paths

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../scss/App.scss';
+
+const App = () => {
+  return (
+    <div className="App__container">
+      <h1 class="app__heading visuallyhidden">Task Timer App</h1>
+    </div>
+  );
+}
+
+export default App;
