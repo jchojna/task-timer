d8eedd1d18664071d4c57ffa76ad8632c1511532
Jakub Chojna <jakub.chojna@gmail.com>
2019-10-25 14:56:17 +0200

    chore(js): change Timer to class component

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,43 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Controls from './Controls';
 import Display from './Display';
 import Break from './Break';
 import Progress from './Progress';
 import '../scss/Timer.scss';
 
-const Timer = (props) => {
-  return (
-    <section className={`Timer ${props.compClassName}`}>
-      <div className="Timer__container">
-        <h2 className="Timer__heading">Work on your task</h2>
-
-        {/* CONTROL BUTTONS */}
-        <Controls
-          isActive={props.isActive}
-          changeDisplayMode={props.changeDisplayMode}
-        />
-
-        {/* TIMER DISPLAY */}
-        <div className="Timer__display">
-          <Display
-            compClassName={props.isElapsedMode
-              ? "Display--visible Display--showUp"
-              : "Display--hideUp"}
+class Timer extends Component {
+  render() {
+    return (
+      <section className={`Timer ${this.props.compClassName}`}>
+        <div className="Timer__container">
+          <h2 className="Timer__heading">Work on your task</h2>
+  
+          {/* CONTROL BUTTONS */}
+          <Controls
+            isActive={this.props.isActive}
+            changeDisplayMode={this.props.changeDisplayMode}
           />
-          <Display
-            compClassName={props.isElapsedMode
-              ? "Display--hideUp"
-              : "Display--visible Display--showUp"}
-          />
+  
+          {/* TIMER DISPLAY */}
+          <div className="Timer__display">
+            <Display
+              compClassName={this.props.isElapsedMode
+                ? "Display--visible Display--showUp"
+                : "Display--hideUp"}
+            />
+            <Display
+              compClassName={this.props.isElapsedMode
+                ? "Display--hideUp"
+                : "Display--visible Display--showUp"}
+            />
+          </div>
+  
+          {/* BREAK */}
+          <Break />
+  
+          {/* PROGRESS */}
+          <Progress />
         </div>
-
-        {/* BREAK */}
-        <Break />
-
-        {/* PROGRESS */}
-        <Progress />
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
 }
 export default Timer;
