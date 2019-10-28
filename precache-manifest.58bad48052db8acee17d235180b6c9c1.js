efbb0f9dad8a087c3f8f4497a431caa13f233fa5
Jakub Chojna <jakub.chojna@gmail.com>
2019-10-28 16:37:05 +0100

    Updates

---
+++ precache-manifest.58bad48052db8acee17d235180b6c9c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d16d76509d9cdab6ad3a995742334bda",
+    "url": "/task-timer/index.html"
+  },
+  {
+    "revision": "efec75bbdd1410a19d67",
+    "url": "/task-timer/static/css/main.32bab239.chunk.css"
+  },
+  {
+    "revision": "bf88570aa0b7c4905857",
+    "url": "/task-timer/static/js/2.093921e3.chunk.js"
+  },
+  {
+    "revision": "efec75bbdd1410a19d67",
+    "url": "/task-timer/static/js/main.541c3ee0.chunk.js"
+  },
+  {
+    "revision": "cc800cc8e14934597498",
+    "url": "/task-timer/static/js/runtime-main.4070a8e8.js"
+  },
+  {
+    "revision": "92f0e3b135c442c8c6ee9df701d4739a",
+    "url": "/task-timer/static/media/icons.92f0e3b1.svg"
+  }
+]);
