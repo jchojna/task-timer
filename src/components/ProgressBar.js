2c7bb430aff7540acbc5ec097b8be9dab1ddb2a3
Jakub Chojna <jakub.chojna@gmail.com>
2019-10-29 18:00:57 +0100

    refactor(js): apply ES6 destructuring to Progress and ProgressBar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -2,19 +2,20 @@ import React from 'react';
 import '../scss/ProgressBar.scss';
 
 const ProgressBar = (props) => {
+  const { isElapsedMode, percentElapsed, percentRemaining } = props;
   return (
     <div className="ProgressBar">
       <div
         className="ProgressBar__part ProgressBar__part--loading"
-        style={{width: `${props.isElapsedMode
-          ? props.percentElapsed
-          : props.percentRemaining}%`}}
+        style={{width: `${isElapsedMode
+          ? percentElapsed
+          : percentRemaining}%`}}
       ></div>
       <div
         className="ProgressBar__part ProgressBar__part--unloading"
-        style={{width: `${props.isElapsedMode
-          ? props.percentRemaining
-          : props.percentElapsed}%`}}
+        style={{width: `${isElapsedMode
+          ? percentRemaining
+          : percentElapsed}%`}}
       ></div>
     </div>
   );
