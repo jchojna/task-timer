ab4592e6435f77ec96280bf6af471a8d2de9fa29
Jakub Chojna <jakub.chojna@gmail.com>
2019-10-29 01:15:56 +0100

    style: set media queries for Time component on mobile devices

--- src/components/Time.js
+++ src/components/Time.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import icons from '../assets/svg/icons.svg';
-import '../scss/Task_Time.scss';
+import '../scss/Task&Time.scss';
 
 const Time = (props) => {
 
@@ -35,60 +35,60 @@ const Time = (props) => {
           <use href={`${icons}#arrow-left`} />
         </svg>
       </button>
-      {/* TIME INPUTS AND START BUTTON */}
-      <div className="Time__row">
-        <input
-          id="task-time"
-          className="Time__input"
-          placeholder="00m00s"
-          maxLength="6"
-          onChange={(e) => {
-            changeTaskTimePlanned(e.target.value);
-            changeState({ alertFlag: true })
-          }}
-          value={taskTimePlanned}
-        />
-        <input
-          id="task-break"
-          className="Time__input"
-          placeholder="00m00s"
-          maxLength="6"
-          onChange={(e) => breakTimePlanned(e.target.value)}
-        />
-        <button
-          className={`Time__start ${isTimerActive
-            ? "Time__start--disabled"
-            : ""}`}
-          onClick={taskTimePlannedValidity && breakTimePlannedValidity
-          ? () => changeState({
-            isTimeVisible: false,
-            isTimerVisible: true,
-            isTaskTimeActive: true,
-            previousTime: Date.now(),
-            taskTimeElapsed: 0,
-            breaksTotal: 0,
-            breakTimeElapsed: 0,
-            breakTimeElapsedArray: breakTimeElapsedResult,
-            alertFlag: false
-          })
-          : undefined}
-          disabled = {isTimerActive}
-        >
-          Start
-        </button>
-      </div>
-      {/* LABELS */}
-      <div className="Time__row">
-        <label className="Time__label Time__label--task" htmlFor="task-time">
-          task time
-        </label>
-        <label className="Time__label Time__label--break" htmlFor="task-break">
-          max break time
-        </label>
-        <p className={`Time__alert ${alertClassName}`}>
-          You have to specify time in the following format: 0m0s
-        </p>
-      </div>
+      {/* TASK TIME INPUT */}
+      <input
+        id="task-time"
+        className="Time__input Time__input--task-time"
+        placeholder="00m00s"
+        maxLength="6"
+        onChange={(e) => {
+          changeTaskTimePlanned(e.target.value);
+          changeState({ alertFlag: true })
+        }}
+        value={taskTimePlanned}
+      />
+      {/* TASK TIME LABEL */}
+      <label className="Time__label Time__label--task-time" htmlFor="task-time">
+        task time
+      </label>
+      {/* BREAK TIME INPUT */}
+      <input
+        id="break-time"
+        className="Time__input Time__input--break-time"
+        placeholder="00m00s"
+        maxLength="6"
+        onChange={(e) => breakTimePlanned(e.target.value)}
+      />
+      {/* BREAK TIME LABEL */}
+      <label className="Time__label Time__label--break-time" htmlFor="break-time">
+        max break time
+      </label>
+      {/* START BUTTON */}
+      <button
+        className={`Time__start ${isTimerActive
+          ? "Time__start--disabled"
+          : ""}`}
+        onClick={taskTimePlannedValidity && breakTimePlannedValidity
+        ? () => changeState({
+          isTimeVisible: false,
+          isTimerVisible: true,
+          isTaskTimeActive: true,
+          previousTime: Date.now(),
+          taskTimeElapsed: 0,
+          breaksTotal: 0,
+          breakTimeElapsed: 0,
+          breakTimeElapsedArray: breakTimeElapsedResult,
+          alertFlag: false
+        })
+        : undefined}
+        disabled = {isTimerActive}
+      >
+        Start
+      </button>
+      {/* ALERT */}
+      <p className={`Time__alert ${alertClassName}`}>
+        You have to specify time in the following format: 0m0s
+      </p>
     </section>
   );
 }
