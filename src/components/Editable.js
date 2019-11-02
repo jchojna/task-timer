36eb9de05c41567750a6e813b129878e89674b8f
Jakub Chojna <jakub.chojna@gmail.com>
2019-11-02 14:07:07 +0100

    feat(Editable): set initial structure and styles for Editable component

--- src/components/Editable.js
+++ src/components/Editable.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import '../scss/Task.scss';
+import '../scss/Editable.scss';
 
 const Editable = (props) => {
   const { className, text } = props;
   
   return (
     <div className={className}>
+      {/* TEXT TITLE */}
       <p className={`${className}__text`}>
         {`"${text}"`}
       </p>
 
+      {/* EDITION ROW */}
+      <div className={`${className}__row`}>
+        <input
+          className={`${className}__input`}
+        />
+        <button>
+          OK
+        </button>
+        <button>
+          EXIT
+        </button>
+      </div>
     </div>
   );
 }
