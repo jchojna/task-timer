9e13a7d93cf4eb8073f1bdf4ab082c759c34fe99
Jakub Chojna <jakub.chojna@gmail.com>
2019-11-21 18:56:43 +0100

    feat(Intro): change Intro into statefull class component

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,55 +1,80 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../scss/Intro.scss';
 
-const Intro = () => {
+class Intro extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
 
-  return (
-    <div className="Intro">
-      <svg className="logo" viewBox="0 0 600 600">
-        {/* MASK */}
-        <mask id="mask" className="logo__mask">
-          <rect width="600" height="600" fill="#fff"/>
+    }
+  }
+
+  handleShadowTranslation = () => {
+
+
+
+  }
+
+  render() {
+    return (
+      <div className="Intro">
+        <svg className="logo" viewBox="0 0 600 600">
+          {/* MASK */}
+          <mask id="mask" className="logo__mask">
+            <rect width="600" height="600" fill="#fff"/>
+            <rect
+              x="200"
+              y="50"
+              width="200"
+              height="35"
+              rx="17.5"
+              ry="17.5"
+              fill="#000"
+            />
+            <circle cx="300" cy="300" r="180" fill="#000"/>
+            <circle cx="300" cy="300" r="145" fill="#fff"/>
+            <rect
+              x="282.5"
+              y="282.5"
+              width="35"
+              height="125"
+              rx="17.5"
+              ry="17.5"
+              fill="#000"
+              className="logo__clockHand"
+            />
+          </mask>
+          {/* ELEMENTS */}
+          <rect width="600" height="600" className="logo__background"/>
           <rect
-            x="200"
-            y="50"
-            width="200"
-            height="35"
-            rx="17.5"
-            ry="17.5"
-            fill="#000"
+            x="199"
+            y="49"
+            width="202"
+            height="37"
+            className="logo__letter logo__letter--top"
           />
-          <circle cx="300" cy="300" r="180" fill="#000"/>
-          <circle cx="300" cy="300" r="145" fill="#fff"/>
+          <circle
+            cx="300"
+            cy="300"
+            r="140"
+            className="logo__letter logo__letter--circle"
+          />
           <rect
-            x="282.5"
-            y="282.5"
-            width="35"
-            height="125"
-            rx="17.5"
-            ry="17.5"
-            fill="#000"
-            className="logo__clockHand"
+            width="600"
+            height="600"
+            className="logo__shadow"
+            mask="url(#mask)"
+            //style={this.handleShadowTranslation}
           />
-        </mask>
-        {/* ELEMENTS */}
-        <rect width="600" height="600" className="logo__background"/>
-        <rect
-          x="199"
-          y="49"
-          width="202"
-          height="37"
-          className="logo__letter logo__letter--top"
-        />
-        <circle
-          cx="300"
-          cy="300"
-          r="140"
-          className="logo__letter logo__letter--circle"
-        />
-        <rect width="600" height="600" className="logo__shadow" mask="url(#mask)"/>
-        <rect width="600" height="600" className="logo__foreground" mask="url(#mask)"/>
-      </svg>
-    </div>
-  );
+          <rect
+            width="600"
+            height="600"
+            className="logo__foreground"
+            mask="url(#mask)"
+          />
+        </svg>
+      </div>
+    );
+  }
 }
 export default Intro;
