12b578bbfa8fc15b051d06461b40720c8fcfe730
Jakub Chojna <jakub.chojna@gmail.com>
2019-11-23 15:02:59 +0100

    feat(Draggable): create Draggable component with empty scss styles

---
+++ src/components/Draggable.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../scss/Draggable.scss';
+
+class Draggable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+export default Draggable;
